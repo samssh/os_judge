3eee9a4b9c9123b724a7863384bed029271a7888
sam sadat shokouhi <sam.sadat.shokouhi@hezardastan.net>
2022-04-06 11:41:51 +0430

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+source venv/bin/activate
+source set-env.sh
+envsubst <docker-compose.yml.sample > docker-compose.yml
+envsubst <rabbitmq/rabbitmq.conf.sample > rabbitmq/rabbitmq.conf
+#docker-compose up -d --build
+#screen -S celery -dm celery -A os_judge worker -l info
