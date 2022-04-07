9e97e30abc79ee2d94e7bf7fc86df63855cbf3fc
sam sadat shokouhi <sam.sadat.shokouhi@hezardastan.net>
2022-04-07 18:55:43 +0430

    improve admin panel of user

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.db.models import Max
 
-# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', 'last_login', 'score')
+
+    @staticmethod
+    def score(user: User):
+        return user.submissions.aggregate(Max('passed_tests'))['passed_tests__max']
+
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
